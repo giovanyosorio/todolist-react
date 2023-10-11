7db33c53636074828ce17a4c493e1b9d1b00ad59
giovany osorio <30839218+giovanyosorio@users.noreply.github.com>
2023-10-11 23:10:42 +0000

    creando el componente Tarea

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import frecodecamplogo from "./imagenes/freecodecamp-logo.png"
+import Tarea from "../src/componentes/Tarea.jsx"
 
 
 import "./App.css"
@@ -7,13 +8,15 @@ function App() {
 
 
   return (
-    <div className='App'>
+    <div className='aplicacion-tareas'>
       <div className='freecodecamp-logo-contenedor'>
         <img className='freecodecamp-logo' src={frecodecamplogo} alt="freecodecamp-logo" />
 
       </div>
       <div className="tareas-lista-principal">
         <h1>Mis tareas</h1>
+          <Tarea texto="Aprender react"/>
+
 
 
       </div>
