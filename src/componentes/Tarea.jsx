1adf49df992ed02c97911a9a0565ce216148073a
giovany osorio <30839218+giovanyosorio@users.noreply.github.com>
2023-10-12 22:53:20 +0000

    estilo tarea completada

--- src/componentes/Tarea.jsx
+++ src/componentes/Tarea.jsx
@@ -4,7 +4,7 @@ import "../stylesheet/Tarea.css"
 import {AiOutlineCloseCircle} from  "react-icons/ai"
 function Tarea({texto,completada}){
     return(
-        <div className={completa ? "tarea-contenedor completada":"tarea-contenedor"}>
+        <div className={completada ? "tarea-contenedor completada":"tarea-contenedor"}>
             <div className="tarea-texto">
                 {texto}
             </div>
